de87165b3cbaa039f23dc5a89520e416e0997c18
niokle <myszapl@gmail.com>
2020-08-19 17:21:19 +0200

    change get to post

--- src/main/java/com/klenio/controller/PasswordController.java
+++ src/main/java/com/klenio/controller/PasswordController.java
@@ -4,18 +4,22 @@ import com.klenio.dto.InputParametersDto;
 import com.klenio.mapper.DtoToDomainMapper;
 import com.klenio.service.SignsDictionaryService;
 import com.klenio.service.PasswordService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin("*")
 @RestController
 @RequestMapping("/v1")
 public class PasswordController {
+    Logger logger = LoggerFactory.getLogger(PasswordController.class);
 
-    @GetMapping("/pass")
+    @PostMapping("/pass")
     @ResponseBody
     public String getPassword(@RequestBody InputParametersDto inputParametersDto, DtoToDomainMapper dtoToDomainMapper) {
         SignsDictionaryService signsDictionaryService = new SignsDictionaryService(dtoToDomainMapper.inputParametersDtoToInputParametersDictionary(inputParametersDto));
         PasswordService passwordService = new PasswordService(dtoToDomainMapper.inputParametersDtoToInputParametersPassword(inputParametersDto), signsDictionaryService);
+        logger.info("Get password");
         return passwordService.getPassword();
     }
 }
