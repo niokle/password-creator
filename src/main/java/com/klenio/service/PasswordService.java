db6c9114160476ad410d7f6734a8f3656951bd6a
niokle <myszapl@gmail.com>
2020-09-25 12:11:47 +0200

    change PasswordService class

--- src/main/java/com/klenio/service/PasswordService.java
+++ src/main/java/com/klenio/service/PasswordService.java
@@ -3,7 +3,10 @@ package com.klenio.service;
 import com.klenio.domain.InputParametersPassword;
 import com.klenio.dto.InputParametersDto;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class PasswordService {
@@ -16,53 +19,44 @@ public class PasswordService {
     }
 
     public String getPassword() {
-        String userName = inputParametersPassword.getUserName();
         String masterPassword = inputParametersPassword.getMasterPassword();
+        String userName = inputParametersPassword.getUserName();
         String appName = inputParametersPassword.getAppName();
         String appAddress = inputParametersPassword.getAppAddress();
         int numberOfSigns = inputParametersPassword.getNumberOfSigns();
 
         List<Character> tableOfSigns = signsDictionaryService.getTableOfSigns();
 
+        List<Integer> passwordMatrix = getPasswordSizeMatrix(numberOfSigns);
+        passwordMatrix = codingPasswordMatrix(passwordMatrix, masterPassword);
+        passwordMatrix = codingPasswordMatrix(passwordMatrix, userName);
+        passwordMatrix = codingPasswordMatrix(passwordMatrix, appName);
+        passwordMatrix = codingPasswordMatrix(passwordMatrix, appAddress);
+
         String password = "";
+        for (Integer i : passwordMatrix) {
+            password += tableOfSigns.get(correctNumber(i, tableOfSigns.size() - 1));
+        }
+        return password;
+    }
 
+    private List<Integer> getPasswordSizeMatrix(int numberOfSigns) {
+        List<Integer> result = new ArrayList<>();
         for (int i = 0; i < numberOfSigns; i++) {
-            password += tableOfSigns.get(correctNumber(i, tableOfSigns.size() -1));
+            result.add((int) (i * Math.PI + numberOfSigns * Math.PI));
         }
+        return result;
+    }
 
-        password = stringsCont(password, masterPassword);
-        password = stringsCont(password,userName);
-        password = stringsCont(password, appName);
-        password = stringsCont(password, appAddress);
-
-        String passwordFinal = "";
-        for (char c : password.toCharArray()) {
-            passwordFinal += tableOfSigns.get(correctNumber(c, tableOfSigns.size() - 1));
+    private List<Integer> codingPasswordMatrix(List<Integer> passwordMatrix, String code) {
+        List<Integer> result = passwordMatrix;
+        for (char c : code.toCharArray()) {
+            result = result.stream().map(val -> val + c * code.length()).collect(Collectors.toList());
         }
-
-        return passwordFinal;
+        return result;
     }
 
     private int correctNumber(int number, int maxNumber) {
         return number%maxNumber == 0 ? maxNumber : number%maxNumber;
-    }
-
-    //todo
-    public String stringsCont(String baseString, String secondSring) {
-        String result = "";
-        int maxValue = Math.max(baseString.length(), secondSring.length());
-        for (int i = 0; i < maxValue; i++) {
-            result += (char) (baseString.toCharArray()[i%baseString.length()] + (secondSring.length() == 0 ? 0 : secondSring.toCharArray()[i%secondSring.length()]));
-        }
-        boolean pasteToEnd = false;
-        while (result.length() != baseString.length()) {
-            pasteToEnd = !pasteToEnd;
-            if (pasteToEnd) {
-                result = result.substring(1, result.length() - 1) + (char) (result.charAt(0) + result.charAt(result.length() - 1));
-            } else {
-                result = (char) (result.charAt(0) + result.charAt(result.length() - 1)) + result.substring(1, result.length() - 1);
-            }
-        }
-        return result;
     }
 }
